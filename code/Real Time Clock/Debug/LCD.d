645274d71fe79e9c681d91a36faa4b858dff9a8a
Mohamed Hazem <mohamedhazemwwe@gmail.com>
2024-05-20 01:25:27 +0300

    Project Done

---
+++ code/Real Time Clock/Debug/LCD.d
@@ -0,0 +1,25 @@
+LCD.d LCD.o: .././LCD.c .././LCD.h .././DIO.h .././LCD_config.h \
+ d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././LCD.h:
+
+.././DIO.h:
+
+.././LCD_config.h:
+
+d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\program\ files\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
